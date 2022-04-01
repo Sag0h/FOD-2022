2ebb0d93f4f97f241cd2d9098c5a9c42b8a7d1e2
Sag0h <mikybrambilla@gmail.com>
2022-04-01 12:19:40 -0300

    termine ejercicio11

--- practica_2/ejercicio_11/ejer11.pas
+++ practica_2/ejercicio_11/ejer11.pas
@@ -47,7 +47,7 @@ begin
 end;
 
 
-procedure crear_maestro(var m: maestro);
+procedure crear_maestro(var m: archivo_maestro);
 var
     d: datos;
 begin
@@ -73,7 +73,7 @@ begin
         assign(v[i], 'd'+str_i);
         rewrite(v[i]);
         leer_datos_detalle(d);
-        while(d.provincia <> '')do begin
+        while(d.info.provincia <> '')do begin
             write(v[i], d);
             leer_datos_detalle(d);
         end;
@@ -81,21 +81,55 @@ begin
     end;
 end;
 
+procedure leer_datos_detalle_archivo(var a: archivo_detalle; var v: datos_detalle);
+begin
+    if(not eof(a))then
+        read(a, v)
+    else
+        v.info.provincia := valor_alto;
+end;
+
+procedure minimo(var v:archivos; var vm:minimos; var min: datos_detalle);
+var
+    i: integer;
+    pos_i: integer;
+begin
+    min.info.provincia := valor_alto;
+    for i:=1 to cant_archivos do begin
+        if(vm[i].info.provincia < min.info.provincia)then begin
+            min:= vm[i];
+            pos_i:= i;
+        end;
+    end;
+    if(min.info.provincia <> valor_alto)then
+        leer_datos_detalle_archivo(v[pos_i], vm[pos_i]);
+end;
+
 procedure actualizar_maestro(var m: archivo_maestro; var v:archivos);
 var
     i: integer;
     v_min: minimos;
     min: datos_detalle;
+    regm: datos;
 begin
     reset(m);
     for i:= 1 to cant_archivos do begin
         reset(v[i]);
-        leer_datos_detalle_archivo(v, vmin);
+        leer_datos_detalle_archivo(v[i], v_min[i]);
     end;
 
-    minimo(v, vmin, min);
-    while(min.provincia <> valor_alto)do begin
-        
+    minimo(v, v_min, min);
+    while(min.info.provincia <> valor_alto)do begin
+        read(m, regm);
+        while(regm.provincia <> min.info.provincia)do
+            read(m, regm);
+        while(min.info.provincia = regm.provincia)do begin
+            regm.cant_alfa := regm.cant_alfa + min.info.cant_alfa;
+            regm.cant_encuestados := regm.cant_encuestados + min.info.cant_encuestados;
+            minimo(v, v_min, min);
+        end;
+        seek(m, filepos(m)-1);
+        write(m, regm);
     end;
 
     for i:= 1 to cant_archivos do begin
@@ -104,11 +138,20 @@ begin
 end;
 
 var 
-    maestro: archivo;
+    d:datos;
+    maestro: archivo_maestro;
     vector: archivos;
 begin
     assign(maestro, 'maestro');
     crear_maestro(maestro);
     crear_detalles(vector);
     actualizar_maestro(maestro, vector);
+    
+    //print de maestro para ver si funciona:
+    // reset(maestro);
+    // while(not eof(maestro))do begin
+    //     read(maestro, d);
+    //     writeln(d.provincia, ' ', d.cant_alfa, ' ', d.cant_encuestados);  
+    // end;
+    // close(maestro);
 end.
